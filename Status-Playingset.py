4464ed24fea6031756c62ad854d09ceac31529a0
amo0721 <46621867+amo0721@users.noreply.github.com>
2019-06-21 18:37:30 +0900

    Update Status-Playingset.py

--- Status-Playingset.py
+++ Status-Playingset.py
@@ -3,3 +3,12 @@ from discord.ext import commands
 import asyncio
 
 bot = commands.bot(command_prefix='/')
+
+@bot.event
+async def on_ready():
+    print('Succees login to {0.user}'.format(bot))
+
+async def playing():
+    await bot.wait_until_ready()
+
+
