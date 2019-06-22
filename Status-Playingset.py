9b0305ef0655e31578fc718ebca04c6f19d1a118
amo0721 <46621867+amo0721@users.noreply.github.com>
2019-06-22 10:33:39 +0900

    Update Status-Playingset.py

--- Status-Playingset.py
+++ Status-Playingset.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
 
 bot = commands.bot(command_prefix='/')
 
@@ -11,4 +12,13 @@ async def on_ready():
 async def playing():
     await bot.wait_until_ready()
 
-    status = [' /도움말 로 명령어를 확인해봐!', '{ }SERVERS | { }USERS'.format(len(bot.guilds), len(set(bot.get_all_members()))), '본 봇은 길드 전다'
+    status = [' /도움말 로 명령어를 확인해봐!', '{ }SERVERS | { }USERS'.format(len(bot.guilds), len(set(bot.get_all_members()))), '본 봇은 길드전용이야!']
+
+    while not bot.is_closed():
+        for i in status:
+            await bot.change_presence(activity=discord.Game(i))
+                await asyncio.sleep(6)
+
+access_token = os.environ["BOT_TOKEN"]
+bot.loop.create_task(playing())  
+bot.run(BOT_TOKEN)
